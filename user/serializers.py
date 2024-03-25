4cda29350082bb428f41d487bad64c22afd4f340
DanSheremeta <danylo3002@gmail.com>
2024-03-25 22:21:36 +0200

    add list of user's orders in user-detail end-point

--- user/serializers.py
+++ user/serializers.py
@@ -1,11 +1,15 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from airport.serializers import OrderListSerializer
+
 
 class UserSerializer(serializers.ModelSerializer):
+    orders = OrderListSerializer(many=True, read_only=True)
+
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "first_name", "last_name", "avatar", "password", "is_staff",)
+        fields = ("id", "email", "first_name", "last_name", "avatar", "orders", "password", "is_staff")
         read_only_fields = ("is_staff", "avatar")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
